78863e328cfed220f7e8c4f981dfa5bb58578542
Jason Xu <jasonxuuuu@gmail.com>
2024-08-01 23:39:19 -0700

    Created function parse_anime to parse the json file retrieved using MAL api.

---
+++ app/json_parser.py
@@ -0,0 +1,36 @@
+from app.models import Anime
+
+
+def parse_anime(anime):
+    """
+    Parses a json document representing an anime and returns an Anime object.
+
+    :param anime: json representation of the anime information
+    :return: Anime object
+    """
+    title = anime['title']
+    image = anime["images"]["jpg"]["image_url"]
+    synopsis = anime["synopsis"]
+    broadcast = anime["broadcast"]
+    genre = parse_genre(anime["genres"])
+
+    return Anime(title, image, synopsis, broadcast, genre)
+
+
+def parse_genre(genres):
+    """
+    Parses a json document representing a genre and returns a list of genre
+
+    :param genres: json representation of genres
+    :return: A list of genre
+    """
+    genre_list = []
+
+    for genre in genres:
+        genre_list.append(genre['name'])
+
+    return genre_list
+
+
+
+
