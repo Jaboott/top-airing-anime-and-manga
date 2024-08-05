f1dd46f815c3067ffd44a8c621420b93a9ea16a1
Jason Xu <jasonxuuuu@gmail.com>
2024-08-04 21:01:03 -0700

    Added scripts to switch the site between the 4 available option.

--- app/static/script.js
+++ app/static/script.js
@@ -1,36 +1,79 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('/anime')
+function switchPage(mode) {
+    const pageTitle = document.querySelector('.title');
+    resetList();
+
+    switch (mode) {
+        case "TA":
+            pageTitle.innerHTML = "Top Anime";
+            loadAnimeList(false);
+            break;
+        case "TM":
+            pageTitle.innerHTML = "Top Manga";
+            loadMangaList(false);
+            break;
+        case "TAA":
+            pageTitle.innerHTML = "Top Airing Anime";
+            loadAnimeList(true);
+            break;
+        case "TAM":
+            pageTitle.innerHTML = "Top Airing Manga";
+            loadMangaList(true);
+            break;
+    }
+}
+
+function loadList(url, isAnime = true) {
+    fetch(url)
         .then(response => response.json())
         .then(data => {
-            const animeList = document.querySelector('.ranking');
-            let i = 1;
-
-            data.forEach(anime => {
-                const animeItem = document.createElement('div');
-                animeItem.classList.add('anime_container');
-
-                const genres = anime.genre.join(', ');
-                const episodes = anime.episodes ? anime.episodes : '???';
-
-                animeItem.innerHTML = `
+            const listContainer = document.querySelector('.ranking');
+            data.forEach((item, index) => {
+                const { title, image, genre, episodes, chapters, score, broadcast, timeline } = item;
+                const genres = genre.join(', ');
+                const count = isAnime ? episodes || '???' : chapters || '???';
+                const countLabel = isAnime ? 'Episodes' : 'Chapters';
+                const timeLabel = isAnime ? `Broadcast: ${broadcast}` : `Timeline: ${timeline}`;
+                
+                // Creating the html div that display the anime/manga
+                const listItem = document.createElement('div');
+                listItem.classList.add('anime_container');
+                listItem.innerHTML = `
                     <div class="rank">
-                        <h1>${i}</h1>
+                        <h1>${index + 1}</h1>
                     </div>
                     <div class="title">
-                        <img src=${anime.image}>
+                        <img src="${image}" alt="${title}">
                         <ul>
-                            <li><h3>${anime.title}</h3></li>
-                            <li><h4>Broadcast: ${anime.broadcast}</h4></li>
+                            <li><h3>${title}</h3></li>
+                            <li><h4>${timeLabel}</h4></li>
                             <li><h4>Genre: ${genres}</h4></li>
-                            <li><h4>${episodes} Episodes</h4></li>
+                            <li><h4>${count} ${countLabel}</h4></li>
                         </ul>
                     </div>
                     <div class="score">
-                        <h1>${anime.score}⭐</h1>
+                        <h1>${score}⭐</h1>
                     </div>
                 `;
-                animeList.appendChild(animeItem);
-                i++;
-            })
-        })
+                listContainer.appendChild(listItem);
+            });
+        });
+}
+
+function loadAnimeList(airing) {
+    const url = airing ? '/anime?airing' : '/anime';
+    loadList(url, true);
+}
+
+function loadMangaList(airing) {
+    const url = airing ? '/manga?airing' : '/manga';
+    loadList(url, false);
+}
+
+function resetList() {
+    const elements = document.querySelectorAll(".anime_container");
+    elements.forEach(element => element.remove());
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadAnimeList(true);
 });
