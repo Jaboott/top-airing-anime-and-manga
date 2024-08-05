38b22712a9b8e1145b43c991397d3cba649f9095
Jason Xu <jasonxuuuu@gmail.com>
2024-08-04 18:30:57 -0700

    Created manga object in models.

--- app/models.py
+++ app/models.py
@@ -26,3 +26,33 @@ class Anime:
             'score': self.score,
             'episodes': self.episodes
         }
+
+
+class Manga:
+    def __init__(self, title, image, synopsis, timeline, genre, score, chapters):
+        self.title = title,
+        self.image = image,
+        self.synopsis = synopsis,
+        self.timeline = timeline,
+        self.genre = genre,
+        self.score = score,
+        self.chapters = chapters
+
+    def __repr__(self):
+        return (f'Manga: {self.title} \n'
+                f'Synopsis: {self.synopsis} \n'
+                f'Timeline: {self.timeline} \n'
+                f'Genre: {self.genre}'
+                f'Score: {self.score} \n'
+                f'Chapters: {self.chapters}')
+
+    def to_dict(self):
+        return {
+            'title': self.title,
+            'image': self.image,
+            'synopsis': self.synopsis,
+            'timeline': self.timeline,
+            'genre': self.genre,
+            'score': self.score,
+            'chapters': self.chapters
+        }
